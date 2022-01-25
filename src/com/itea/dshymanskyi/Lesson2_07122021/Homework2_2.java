612fdf28459dd2ae37c8c4c3727ac83a65ea3a90
dish <noob4ikua@gmail.com>
2022-01-26 01:47:31 +0200

    Lesson2 Task 2 added.

---
+++ src/com/itea/dshymanskyi/Lesson2_07122021/Homework2_2.java
@@ -0,0 +1,14 @@
+package com.itea.dshymanskyi.Lesson2_07122021;
+
+/*Задача №2
+Необходимо вывести в консоль цифры от 5 до 1. В консоли должно будет:
+
+5 4 3 2 1 */
+
+public class Homework2_2 {
+    public static void main(String[] args) {
+        for (int i = 5; i >= 1; i--) {
+            System.out.print(i + " ");
+        }
+    }
+}
