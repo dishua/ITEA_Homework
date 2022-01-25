16b3e5abd216d77fec128aaa134988806ed807b9
dish <noob4ikua@gmail.com>
2022-01-26 01:49:59 +0200

    Lesson2 Task 5 added.

---
+++ src/com/itea/dshymanskyi/Lesson2_07122021/Homework2_5.java
@@ -0,0 +1,15 @@
+package com.itea.dshymanskyi.Lesson2_07122021;
+
+/*Задача №5
+Необходимо, чтобы программа выводила на экран такую последовательность:
+7 14 21 28 35 42 49 56 63 70 77 84 91 98 */
+
+public class Homework2_5 {
+    public static void main(String[] args) {
+
+        for (int i = 7; i <= 98; i += 7) {
+            System.out.print(i + " ");
+        }
+
+    }
+}
