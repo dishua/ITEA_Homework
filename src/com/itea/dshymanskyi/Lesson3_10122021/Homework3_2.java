4575845a74e1ae55344e85205492350f8fb13f24
dish <noob4ikua@gmail.com>
2022-01-26 02:08:52 +0200

    Lesson3 Task 2 added.

---
+++ src/com/itea/dshymanskyi/Lesson3_10122021/Homework3_2.java
@@ -0,0 +1,11 @@
+package com.itea.dshymanskyi.Lesson3_10122021;
+
+public class Homework3_2 {
+    public static void main(String[] args) {
+        String first = "PHP Exercises";
+        String second = "and Python Exercises";
+        String joinedString = String.join(" ",first,second);
+
+        System.out.println(joinedString);
+    }
+}
